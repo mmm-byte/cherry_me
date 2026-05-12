0cb4f72649d6b42d65a964cd85a428b9b0b1c6ff
Zenith <mahindragk22@gmail.com>
2026-05-12 16:53:29 -0500

    Add files via upload

---
+++ rush1-4.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'rush-1-4'))
+from rush import rush
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Invalid size", file=sys.stderr)
+        sys.exit(1)
+    try:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+        rush(x, y)
+    except ValueError:
+        print("Invalid size", file=sys.stderr)
+        sys.exit(1)
